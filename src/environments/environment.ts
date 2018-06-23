75724ee4859df19518c0af071687d3ffac270f93
ardnahcivar <ravbhanage@gmail.com>
2018-06-23 08:06:18 +0530

    removed firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,6 @@
 export const environment = {
   production: false,
   firebase: {
-   apiKey: "AIzaSyALh4oQfqy4VXDyKRCXB8--7_NNEK-Q0HY",
-   authDomain: "my-awesome-note.firebaseapp.com",
-   databaseURL: "https://my-awesome-note.firebaseio.com",
-   projectId: "my-awesome-note",
-   storageBucket: "my-awesome-note.appspot.com",
-   messagingSenderId: "1067273660013"
+  
  }
 };
